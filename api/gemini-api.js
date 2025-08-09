9184ca7c627c038e37e89f3d12249269123bf1bf
Gibshosting <gibrandwisatrio1305@gmail.com>
2025-08-10 00:37:07 +0700

    Update gemini-api.js

--- api/gemini-api.js
+++ api/gemini-api.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
 
   const { prompt } = req.body;
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+  const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro"});
 
   try {
     const result = await model.generateContent(prompt);
